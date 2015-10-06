a4f57582354e88ea75e24eeea74942632ec6c78e
Gevorg Harutyunyan <gevharut@gmail.com>
2015-10-06 08:41:22 +0400

    Chart init

--- src/app/app-controller.js
+++ src/app/app-controller.js
@@ -1,62 +1,160 @@
 pm.main
-.controller("AppCtrl", function($log, $rootScope, dpd) {
+.controller("AppCtrl", function($log, $rootScope, $timeout, $scope, dpd) {
 	var vm = this;
 
-	vm.products = [];
+	vm.filter = {
+		dateFrom: moment().subtract('days', 30).toDate(),
+		dateTo: moment().toDate()
+	};
 
-	dpd.products.get()
-	.then(function(res) {
+	vm.data = [];
+	vm.products = [];
+	vm.entries = [];
+
+	dpd.products.get().then(function(res) {
 		vm.products = res.data.map(function(product) {
 			product.isSelected = false;
 			return product;
 		});
 	});
 
-	setData();
+	dpd.entries.get().then(function(res) {
+		vm.entries = res.data;
+
+		$log.log("Entries:", vm.entries);
+	});
+
+	// On products change, set data
+	$scope.$watch(function() {
+		return vm.products;
+	}, function() {
+		setData();
+	});
+
+
+	$scope.$watch(function() {
+		return vm.entries;
+	}, function() {
+		setData();
+	});
+
+
+	vm.queryItems = function() {
+
+	};
+
+
+	// $timeout(function() {
+	// 	vm.options = {
+	// 		colors: [
+	// 			"#77BFD7", 
+	// 			"#A8D6E6"
+	// 		],
+	// 		labels: ["a", "b"],
+	// 		x: "x",
+	// 		y: ["a", "b"]
+	// 	};
+
+	// 	vm.data = [
+	// 		{
+	// 			x: "2006",
+	// 			a: 1,
+	// 			b: 1
+	// 		},
+	// 		{
+	// 			x: "2007",
+	// 			a: 1,
+	// 			b: 1
+	// 		}
+	// 	];
+	// }, 1000);
+
+
+	// $timeout(function() {
+	// 	vm.options = {
+	// 		colors: [
+	// 			"#77BFD7", 
+	// 			"#A8D6E6"
+	// 		],
+	// 		labels: ["a", "b"],
+	// 		x: "x",
+	// 		y: ["a", "b"]
+	// 	};
+
+	// 	vm.data = [
+	// 		{
+	// 			x: "2006",
+	// 			a: 11,
+	// 			b: 1
+	// 		},
+	// 		{
+	// 			x: "2007",
+	// 			a: 11,
+	// 			b: 1
+	// 		}
+	// 	];
+	// }, 5000);
 
 
 	function setData() {
-		vm.options = {
-			colors: [
-				"#77BFD7", 
-				"#A8D6E6"
-			],
-			labels: [
-				"Series A", 
-				"Series B"
-			],
-			x: "x",
-			// y: ["1", "2"]
-			y: ["a", "b"]
-		};
 
-		vm.data = generateRandomData();
-	}
-
-
-	function generateRandomData() {
-
-		var res = [];
-
-		var today = moment();
-		var monthAgo = moment().subtract(30, 'days');
-
-		var i = 1;
-
-		for (var date = monthAgo; date.isBefore(today); date.add(1, 'day')) {
-
-
-			res.push({
-				// date: date.toDate().toString(),
-				x: i,
-				a: Math.random() * 1500,
-				b: Math.random() * 400
-			});
-
-			i++;
+		if (!vm.products.length || !vm.entries.length) {
+			return false;
 		}
 
-		return res;
+		var dateArr = {};
+		
+
+		vm.entries.forEach(function(val) {
+			if (typeof dateArr[val.date] == 'undefined') {
+				dateArr[val.date] = [];
+			}
+			var obj = {
+				productid: val.productid,
+				price: val.price
+			};
+			dateArr[val.date].push(obj);
+		});
+
+		var finalArr = [];
+		for (var key in dateArr) {
+			var obj = {
+				x: parseInt(key)
+			};
+
+			dateArr[key].forEach(function(val) {
+				obj[val.productid] = val.price;
+			});
+
+			finalArr.push(obj);
+		}
+
+		// Y Keys based on data
+
+		var yKeys = [];
+
+		for (var key in finalArr[0]) {
+			if (key !== "x") {
+				yKeys.push(key);
+			}
+		}
+
+
+		vm.options = {
+			// colors: [
+			// 	"#77BFD7", 
+			// 	"#A8D6E6"
+			// ],
+			labels: yKeys,
+			x: "x",
+			y: yKeys
+		};
+
+		vm.data = finalArr;
+
+
+		$log.log("Options:", vm.options);
+		$log.log("Data", vm.data);
 	}
 	
 });
